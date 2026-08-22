f0460830ad2dd25753f633ea61df83ba4ee84dc9
Mr Kahanda <elifaskahanda10@gmail.com>
2026-08-22 15:47:00 +0200

    Finished with everything on the home page

--- script.js
+++ script.js
@@ -5,3 +5,28 @@ const  dark_btn = document.getElementById("dark-btn");
 function settingsMenuToggle() {
     settings_menu.classList.toggle("settings-menu-height");
 }
+
+dark_btn.onclick = function(){
+    dark_btn.classList.toggle("dark-btn-on");
+    document.body.classList.toggle("dark-theme");
+
+    if (localStorage.getItem("theme") == "light") {
+        localStorage.setItem("theme", "dark");
+    }
+    else {
+        localStorage.setItem("theme", "light");
+    }
+}
+
+
+if (localStorage.getItem("theme") == "light"){
+    dark_btn.classList.remove("dark-btn-on");
+    document.body.classList.remove("dark-theme");
+}
+else if (localStorage.getItem("theme") == "dark") {
+    dark_btn.classList.add("dark-btn-on");
+    document.body.classList.add("dark-theme"); 
+}
+else {
+    localStorage.setItem("theme", "light");
+}
