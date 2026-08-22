28f4128a9b09861e3d8ae389d8690cacba73d5d4
Mr Kahanda <elifaskahanda10@gmail.com>
2026-08-22 15:04:18 +0200

    Working on the dark theme and light theme toggle

---
+++ script.js
@@ -0,0 +1,7 @@
+
+const settings_menu = document.querySelector(".settings-menu");
+const  dark_btn = document.getElementById("dark-btn");
+
+function settingsMenuToggle() {
+    settings_menu.classList.toggle("settings-menu-height");
+}
